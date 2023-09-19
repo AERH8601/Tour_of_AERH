3b43751c312d5432bd4a81a5570ce845d5ea5273
Andres RH <andres.rossi@ueda.edu.co>
2023-09-19 12:13:10 -0500

    Se establece lista y selección de heroes con su descripción

---
+++ tour-of-aerh/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'El Bananero' },
+  { id: 13, name: 'Gota de lluvia' },
+  { id: 14, name: 'El polaco' },
+  { id: 15, name: 'Floricienta' },
+  { id: 16, name: 'El Mamut' },
+  { id: 17, name: 'El Loco' },
+  { id: 18, name: 'Mr Tucan' },
+  { id: 19, name: 'El Burro' },
+  { id: 20, name: 'La chiky' }
+];
